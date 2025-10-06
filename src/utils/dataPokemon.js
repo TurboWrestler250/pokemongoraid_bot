02afee9aa41e4e19f6188c45018d027c5ea74512
Alessandro Mamolo <alex.mamolo@gmx.com>
2025-10-06 11:06:45 +0200

    modifica di costante da globale a interno alla funzione

--- src/utils/dataPokemon.js
+++ src/utils/dataPokemon.js
@@ -1,6 +1,5 @@
-const API_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
-
 export async function dataPokemon(pokemon) {
+    const API_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
     const res = await fetch(API_POKEMON + pokemon.toLowerCase());
     if (!res.ok) throw new Error("Pokémon non trovato");
     const dataPokemon = await res.json();
