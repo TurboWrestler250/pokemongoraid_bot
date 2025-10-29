fd0192c43165f171138dfbb6e08a95302a0e8e87
Alessandro Mamolo <71407691+TurboWrestler250@users.noreply.github.com>
2025-10-29 22:37:57 +0100

    gestione errore nella chiamata API

--- src/utils/dataPokemon.js
+++ src/utils/dataPokemon.js
@@ -2,9 +2,18 @@ export async function dataPokemon(pokemon) {
     if (pokemon === "shaymin") pokemon = "shaymin-land";
     if (pokemon === "deoxys") pokemon = "deoxys-normal";
     if (pokemon === "giratina") pokemon = "giratina-altered";
-    const API_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
-    const res = await fetch(API_POKEMON + pokemon.toLowerCase());
-    if (!res.ok) throw new Error("Pokémon non trovato");
-    const dataPokemon = await res.json();
-    return dataPokemon;
+
+    try {
+        const API_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
+        const res = await fetch(API_POKEMON + pokemon.toLowerCase());
+        if (!res.ok) throw new Error("Pokémon non trovato");
+        const dataPokemon = await res.json();
+        return dataPokemon;
+    } catch (err) {
+        console.error("Errore durante la chiamata API per numero di pokedex: ", err);
+        return ctx.answerCallbackQuery({ 
+            text: "❌ Pokémon non trovato nel database", 
+            show_alert: true 
+        });
+    }
 }
