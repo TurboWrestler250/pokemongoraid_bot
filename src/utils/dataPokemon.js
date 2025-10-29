19175e63c1b54b1a49ea2dff95cef9f9e6b7fa36
Alessandro Mamolo <71407691+TurboWrestler250@users.noreply.github.com>
2025-10-29 18:59:30 +0100

    controllo su Giratina in api_pokemon di pokeapi.co

--- src/utils/dataPokemon.js
+++ src/utils/dataPokemon.js
@@ -1,5 +1,7 @@
 export async function dataPokemon(pokemon) {
+    if (pokemon === "shaymin") pokemon = "shaymin-land";
     if (pokemon === "deoxys") pokemon = "deoxys-normal";
+    if (pokemon === "giratina") pokemon = "giratina-altered";
     const API_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
     const res = await fetch(API_POKEMON + pokemon.toLowerCase());
     if (!res.ok) throw new Error("Pokémon non trovato");
